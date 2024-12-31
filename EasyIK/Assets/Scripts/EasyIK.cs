17dadf008a2bcf3e993278dfe2717a74288414d0
Jack Humberstone <160753124+SwagAccount@users.noreply.github.com>
2024-12-31 03:23:07 +0000

    Update EasyIK.cs

--- EasyIK/Assets/Scripts/EasyIK.cs
+++ EasyIK/Assets/Scripts/EasyIK.cs
@@ -1,80 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-using System.Linq;
-
-public class EasyIK : MonoBehaviour
+﻿
+public class EasyIK : Component, Component.ExecuteInEditor
 {
-    [Header("IK properties")]
-    public int numberOfJoints = 2;
-    public Transform ikTarget;
-    public int iterations = 10;
-    public float tolerance = 0.05f;
-    private Transform[] jointTransforms;
-    private Vector3 startPosition;
-    private Vector3[] jointPositions;
-    private float[] boneLength;
-    private float jointChainLength;
-    private float distanceToTarget;
-    private Quaternion[] startRotation;
-    private Vector3[] jointStartDirection;
-    private Quaternion ikTargetStartRot;
-    private Quaternion lastJointStartRot;
+	[Property] public int numberOfJoints { get; set; } = 2;
+	[Property] public GameObject ikTarget { get; set; }
+	[Property] public int iterations { get; set; } = 10;
+	[Property] public float tolerance { get; set; } = 0.05f;
+	private GameObject[] jointTransforms { get; set; }
+    private Vector3 startPosition { get; set; }
+	[Property] private Vector3[] jointPositions { get; set; }
+    private float[] boneLength { get; set; }
+    private float jointChainLength { get; set; }
+    private float distanceToTarget { get; set; }
+    private Rotation[] startRotation { get; set; }
+    private Vector3[] jointStartDirection { get; set; }
+    private Rotation ikTargetStartRot { get; set; }
+    private Rotation lastJointStartRot { get; set; }
 
-    [Header("Pole target (3 joint chain)")]
-    public Transform poleTarget;
+	[Property] public GameObject poleTarget;
 
-    [Header("Debug")]
-    public bool debugJoints = true;
-    public bool localRotationAxis = false;
+	[Property] public bool debugJoints = true;
+	[Property] public bool localRotationAxis = false;
 
     // Remove this if you need bigger gizmos:
-    [Range(0.0f, 1.0f)]
-    public float gizmoSize = 0.05f;
+    [Property, Range(0.0f, 40f)]
+    public float gizmoSize = 2f;
+
     public bool poleDirection = false;
     public bool poleRotationAxis = false;
 
-    void Awake()
+	protected override void OnStart()
+	{
+		Awake();
+	}
+	protected override async void OnEnabled()
+	{
+		Awake();
+	}
+	void Awake()
     {
         // Let's set some properties
         jointChainLength = 0;
-        jointTransforms = new Transform[numberOfJoints];
+        jointTransforms = new GameObject[numberOfJoints];
         jointPositions = new Vector3[numberOfJoints];
         boneLength = new float[numberOfJoints];
         jointStartDirection = new Vector3[numberOfJoints];
-        startRotation = new Quaternion[numberOfJoints];
-        ikTargetStartRot = ikTarget.rotation;
+        startRotation = new Rotation[numberOfJoints];
+        ikTargetStartRot = ikTarget.WorldRotation;
 
-        var current = transform;
+        var current = GameObject;
 
         // For each bone calculate and store the lenght of the bone
-        for (var i = 0; i < jointTransforms.Length; i += 1)
+        for (var i = 0; i < jointTransforms.Length; i++)
         {
             jointTransforms[i] = current;
             // If the bones lenght equals the max lenght, we are on the last joint in the chain
             if (i == jointTransforms.Length - 1)
             {
-                lastJointStartRot = current.rotation;
+                lastJointStartRot = current.WorldRotation;
                 return;
             }
             // Store length and add the sum of the bone lengths
             else
             {
-                boneLength[i] = Vector3.Distance(current.position, current.GetChild(0).position);
+                boneLength[i] = Vector3.DistanceBetween(current.WorldPosition, current.Children[0].WorldPosition );
                 jointChainLength += boneLength[i];
 
-                jointStartDirection[i] = current.GetChild(0).position - current.position;
-                startRotation[i] = current.rotation;
+                jointStartDirection[i] = current.Children[0].WorldPosition - current.WorldPosition;
+                startRotation[i] = current.WorldRotation;
             }
             // Move the iteration to next joint in the chain
-            current = current.GetChild(0);
+            current = current.Children[0];
         }
-    }
-
-    void Start()
-    {
-
     }
 
     void PoleConstraint()
@@ -82,54 +78,63 @@ public class EasyIK : MonoBehaviour
         if (poleTarget != null && numberOfJoints < 4)
         {
             // Get the limb axis direction
-            var limbAxis = (jointPositions[2] - jointPositions[0]).normalized;
+            var limbAxis = (jointPositions[2] - jointPositions[0]).Normal;
 
             // Get the direction from the root joint to the pole target and mid joint
-            Vector3 poleDirection = (poleTarget.position - jointPositions[0]).normalized;
-            Vector3 boneDirection = (jointPositions[1] - jointPositions[0]).normalized;
+            Vector3 poleDirection = (poleTarget.WorldPosition - jointPositions[0]).Normal;
+            Vector3 boneDirection = (jointPositions[1] - jointPositions[0]).Normal;
             
             // Ortho-normalize the vectors
-            Vector3.OrthoNormalize(ref limbAxis, ref poleDirection);
-            Vector3.OrthoNormalize(ref limbAxis, ref boneDirection);
+            OrthoNormalize(ref limbAxis, ref poleDirection);
+            OrthoNormalize(ref limbAxis, ref boneDirection);
 
             // Calculate the angle between the boneDirection vector and poleDirection vector
-            Quaternion angle = Quaternion.FromToRotation(boneDirection, poleDirection);
+            Rotation angle = Rotation.FromToRotation(boneDirection, poleDirection);
 
             // Rotate the middle bone using the angle
             jointPositions[1] = angle * (jointPositions[1] - jointPositions[0]) + jointPositions[0];
         }
     }
 
-    void Backward()
+	public static void OrthoNormalize( ref Vector3 normal, ref Vector3 tangent )
+	{
+		normal = normal.Normal;
+
+		tangent -= Vector3.Dot( tangent, normal ) * normal;
+
+		tangent = tangent.Normal;
+	}
+
+	void Backward()
     {
-        // Iterate through every position in the list until we reach the start of the chain
+        // Iterate through every WorldPosition in the list until we reach the start of the chain
         for (int i = jointPositions.Length - 1; i >= 0; i -= 1)
         {
-            // The last bone position should have the same position as the ikTarget
+            // The last bone WorldPosition should have the same WorldPosition as the ikTarget
             if (i == jointPositions.Length - 1)
             {
-                jointPositions[i] = ikTarget.transform.position;
+                jointPositions[i] = ikTarget.WorldPosition;
             }
             else
             {
-                jointPositions[i] = jointPositions[i + 1] + (jointPositions[i] - jointPositions[i + 1]).normalized * boneLength[i];
+                jointPositions[i] = jointPositions[i + 1] + (jointPositions[i] - jointPositions[i + 1]).Normal * boneLength[i];
             }
         }
     }
 
     void Forward()
     {
-        // Iterate through every position in the list until we reach the end of the chain
+        // Iterate through every WorldPosition in the list until we reach the end of the chain
         for (int i = 0; i < jointPositions.Length; i += 1)
         {
-            // The first bone position should have the same position as the startPosition
+            // The first bone WorldPosition should have the same WorldPosition as the startPosition
             if (i == 0)
             {
                 jointPositions[i] = startPosition;
             }
             else
             {
-                jointPositions[i] = jointPositions[i - 1] + (jointPositions[i] - jointPositions[i - 1]).normalized * boneLength[i - 1];
+                jointPositions[i] = jointPositions[i - 1] + (jointPositions[i] - jointPositions[i - 1]).Normal * boneLength[i - 1];
             }
         }
     }
@@ -139,28 +144,28 @@ public class EasyIK : MonoBehaviour
         // Get the jointPositions from the joints
         for (int i = 0; i < jointTransforms.Length; i += 1)
         {
-            jointPositions[i] = jointTransforms[i].position;
+            jointPositions[i] = jointTransforms[i].WorldPosition;
         }
         // Distance from the root to the ikTarget
-        distanceToTarget = Vector3.Distance(jointPositions[0], ikTarget.position);
+        distanceToTarget = Vector3.DistanceBetween(jointPositions[0], ikTarget.WorldPosition);
 
         // IF THE TARGET IS NOT REACHABLE
         if (distanceToTarget > jointChainLength)
         {
             // Direction from root to ikTarget
-            var direction = ikTarget.position - jointPositions[0];
+            var direction = ikTarget.WorldPosition - jointPositions[0];
 
             // Get the jointPositions
             for (int i = 1; i < jointPositions.Length; i += 1)
             {
-                jointPositions[i] = jointPositions[i - 1] + direction.normalized * boneLength[i - 1];
+                jointPositions[i] = jointPositions[i - 1] + direction.Normal * boneLength[i - 1];
             }
         }
         // IF THE TARGET IS REACHABLE
         else
         {
             // Get the distance from the leaf bone to the ikTarget
-            float distToTarget = Vector3.Distance(jointPositions[jointPositions.Length - 1], ikTarget.position);
+            float distToTarget = Vector3.DistanceBetween(jointPositions[jointPositions.Length - 1], ikTarget.WorldPosition);
             float counter = 0;
             // While the distance to target is greater than the tolerance let's iterate until we are close enough
             while (distToTarget > tolerance)
@@ -182,49 +187,47 @@ public class EasyIK : MonoBehaviour
         // Apply the jointPositions and rotations to the joints
         for (int i = 0; i < jointPositions.Length - 1; i += 1)
         {
-            jointTransforms[i].position = jointPositions[i];
-            var targetRotation = Quaternion.FromToRotation(jointStartDirection[i], jointPositions[i + 1] - jointPositions[i]);
-            jointTransforms[i].rotation = targetRotation * startRotation[i];
+            jointTransforms[i].WorldPosition = jointPositions[i];
+            var targetRotation = Rotation.FromToRotation(jointStartDirection[i], jointPositions[i + 1] - jointPositions[i]);
+            jointTransforms[i].WorldRotation = targetRotation * startRotation[i];
         }
         // Let's constrain the rotation of the last joint to the IK target and maintain the offset.
-        Quaternion offset = lastJointStartRot * Quaternion.Inverse(ikTargetStartRot);
-        jointTransforms.Last().rotation = ikTarget.rotation * offset;
+        Rotation offset = lastJointStartRot * ikTargetStartRot.Inverse;
+        jointTransforms.Last().WorldRotation = ikTarget.WorldRotation * offset;
     }
 
-    void Update()
+    protected override void OnUpdate()
     {
         SolveIK();
     }
 
     // Visual debugging
-    void OnDrawGizmos()
-    {   
+    protected override void DrawGizmos()
+    {
         if (debugJoints == true)
         {   
-            var current = transform;
-            var child = transform.GetChild(0);
-
+            var current = GameObject;
+            var child = GameObject.Children[0];
+			Gizmo.Draw.Color = Color.Cyan;
             for (int i = 0; i < numberOfJoints; i += 1)
             {
-                if (i == numberOfJoints - 2)
+                if (i == numberOfJoints - 1)
                 {
-                    var length = Vector3.Distance(current.position, child.position);
-                    DrawWireCapsule(current.position + (child.position - current.position).normalized * length / 2, Quaternion.FromToRotation(Vector3.up, (child.position - current.position).normalized), gizmoSize, length, Color.cyan);
-                    break;
+					Gizmo.Draw.LineCapsule( new Capsule( WorldTransform.PointToLocal(current.WorldPosition), WorldTransform.PointToLocal( child.WorldPosition ), gizmoSize) );
+			break;
                 }
                 else
                 {
-                    var length = Vector3.Distance(current.position, child.position);
-                    DrawWireCapsule(current.position + (child.position - current.position).normalized * length / 2, Quaternion.FromToRotation(Vector3.up, (child.position - current.position).normalized), gizmoSize, length, Color.cyan);
-                    current = current.GetChild(0);
-                    child = current.GetChild(0);
+					Gizmo.Draw.LineCapsule( new Capsule(WorldTransform.PointToLocal( current.WorldPosition ), WorldTransform.PointToLocal( child.WorldPosition ), gizmoSize) );
+					current = current.Children[0];
+                    child = current.Children[0];
                 }
             }
         }
 
         if (localRotationAxis == true)
         {    
-            var current = transform;
+            var current = GameObject;
             for (int i = 0; i < numberOfJoints; i += 1)
             {
                 if (i == numberOfJoints - 1)
@@ -234,62 +237,50 @@ public class EasyIK : MonoBehaviour
                 else
                 {
                     drawHandle(current);
-                    current = current.GetChild(0);
+                    current = current.Children[0];
                 }
             }
         }
 
-        var start = transform;
-        var mid = start.GetChild(0);
-        var end = mid.GetChild(0);
+        var start = GameObject;
+		var mid = start.Children[0];
+        var end = mid.Children[0];
 
-        if (poleRotationAxis == true && poleTarget != null && numberOfJoints < 4)
+		if (poleRotationAxis == true && poleTarget != null && numberOfJoints < 4)
         {    
-            Handles.color = Color.white;
-            Handles.DrawLine(start.position, end.position);
+            Gizmo.Draw.Color = Color.White;
+            Gizmo.Draw.Line(start.WorldPosition, end.WorldPosition);
         }
 
         if (poleDirection == true && poleTarget != null && numberOfJoints < 4)
-        {    
-            Handles.color = Color.grey;
-            Handles.DrawLine(start.position, poleTarget.position);
-            Handles.DrawLine(end.position, poleTarget.position);
+        {
+			Gizmo.Draw.Color = Color.Gray;
+			Gizmo.Draw.Line( start.WorldPosition, poleTarget.WorldPosition);
+			Gizmo.Draw.Line( end.WorldPosition, poleTarget.WorldPosition);
         }
 
-    }
+		if(ikTarget.IsValid())
+		{
+			Gizmo.Draw.Color = Color.Green;
+			Gizmo.Draw.SolidSphere( WorldTransform.PointToLocal(ikTarget.WorldPosition), gizmoSize );
+		}
 
-    void drawHandle(Transform debugJoint)
+		if(poleTarget.IsValid())
+		{
+			Gizmo.Draw.Color = Color.Red;
+			Gizmo.Draw.SolidSphere( WorldTransform.PointToLocal(  poleTarget.WorldPosition ), gizmoSize );
+		}
+	}
+
+    void drawHandle(GameObject debugJoint)
     {
-        Handles.color = Handles.xAxisColor;
-        Handles.ArrowHandleCap(0, debugJoint.position, debugJoint.rotation * Quaternion.LookRotation(Vector3.right), gizmoSize, EventType.Repaint);
-                
-        Handles.color = Handles.yAxisColor;
-        Handles.ArrowHandleCap(0, debugJoint.position, debugJoint.rotation * Quaternion.LookRotation(Vector3.up), gizmoSize, EventType.Repaint);
+        Gizmo.Draw.Color = Color.Red;
+		Gizmo.Draw.Arrow( WorldTransform.PointToLocal( debugJoint.WorldPosition ), WorldTransform.PointToLocal( debugJoint.WorldPosition + Vector3.Forward * gizmoSize ), gizmoSize * 0.1f, gizmoSize * 0.1f );
 
-        Handles.color = Handles.zAxisColor;
-        Handles.ArrowHandleCap(0, debugJoint.position, debugJoint.rotation * Quaternion.LookRotation(Vector3.forward), gizmoSize, EventType.Repaint);
-    }
+		Gizmo.Draw.Color = Color.Yellow;
+		Gizmo.Draw.Arrow( WorldTransform.PointToLocal( debugJoint.WorldPosition ), WorldTransform.PointToLocal( debugJoint.WorldPosition + Vector3.Right * gizmoSize ), gizmoSize * 0.1f, gizmoSize * 0.1f );
 
-     public static void DrawWireCapsule(Vector3 _pos, Quaternion _rot, float _radius, float _height, Color _color = default(Color))
-     {
-        Handles.color = _color;
-        Matrix4x4 angleMatrix = Matrix4x4.TRS(_pos, _rot, Handles.matrix.lossyScale);
-        using (new Handles.DrawingScope(angleMatrix))
-        {
-            var pointOffset = (_height - (_radius * 2)) / 2;
-
-            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.left, Vector3.back, -180, _radius);
-            Handles.DrawLine(new Vector3(0, pointOffset, -_radius), new Vector3(0, -pointOffset, -_radius));
-            Handles.DrawLine(new Vector3(0, pointOffset, _radius), new Vector3(0, -pointOffset, _radius));
-            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.left, Vector3.back, 180, _radius);
-
-            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.back, Vector3.left, 180, _radius);
-            Handles.DrawLine(new Vector3(-_radius, pointOffset, 0), new Vector3(-_radius, -pointOffset, 0));
-            Handles.DrawLine(new Vector3(_radius, pointOffset, 0), new Vector3(_radius, -pointOffset, 0));
-            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.back, Vector3.left, -180, _radius);
-
-            Handles.DrawWireDisc(Vector3.up * pointOffset, Vector3.up, _radius);
-            Handles.DrawWireDisc(Vector3.down * pointOffset, Vector3.up, _radius);
-         }
-     }
+		Gizmo.Draw.Color = Color.Blue;
+		Gizmo.Draw.Arrow( WorldTransform.PointToLocal( debugJoint.WorldPosition ), WorldTransform.PointToLocal( debugJoint.WorldPosition + Vector3.Up * gizmoSize ), gizmoSize * 0.1f, gizmoSize * 0.1f );
+	}
 }
